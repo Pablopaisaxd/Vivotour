894ab3e629e9b2c886e0629ba63dbecef9d5f4d1
JUANVAL87 <cr7juanmax@gmail.com>
2025-05-09 11:45:09 -0500

    Texto cambiante

--- app.js
+++ app.js
@@ -17,3 +17,24 @@ if (valor=="true") {
         modo.innerHTML='<i class="fa-solid fa-moon"></i>';
     modo.style.color="black"
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    const textos = document.querySelectorAll(".pint");
+    let index = 0;
+
+    function cambiarTexto() {
+        textos.forEach((p) => {
+            p.style.opacity = "0";
+            p.style.visibility = "hidden";
+            p.style.position = "absolute";
+        });
+
+        textos[index].style.opacity = "1";
+        textos[index].style.visibility = "visible";
+        textos[index].style.position = "static";
+
+        index = (index + 1) % textos.length;
+    }
+
+    setInterval(cambiarTexto, 10000);
+});
